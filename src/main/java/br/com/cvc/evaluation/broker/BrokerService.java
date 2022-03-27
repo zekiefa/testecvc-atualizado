f74fa92066778cf2203e89b47e8443ef7b90817c
JOAO PAULO BRANDAO DA SILVA <joao.silva-ext@viavarejo.com.br>
2022-03-27 10:51:50 -0300

    Removing unused imports

--- src/main/java/br/com/cvc/evaluation/broker/BrokerService.java
+++ src/main/java/br/com/cvc/evaluation/broker/BrokerService.java
@@ -5,11 +5,9 @@ import java.util.Objects;
 import java.util.Optional;
 
 import br.com.cvc.evaluation.broker.dto.BrokerHotel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
