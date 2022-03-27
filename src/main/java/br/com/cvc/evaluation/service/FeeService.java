29afb0e9406309fbc185a8e3192cdcdb117d9f2d
JOAO PAULO BRANDAO DA SILVA <joao.silva-ext@viavarejo.com.br>
2022-03-27 10:03:54 -0300

    Adding logs

--- src/main/java/br/com/cvc/evaluation/service/FeeService.java
+++ src/main/java/br/com/cvc/evaluation/service/FeeService.java
@@ -2,9 +2,11 @@ package br.com.cvc.evaluation.service;
 
 import java.math.BigDecimal;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class FeeService {
 	// o ideal é que a comissão seja parametrizada via banco de dados
@@ -16,8 +18,11 @@ public class FeeService {
 	}
 
 	public BigDecimal calculateFee(final BigDecimal paxPrice, final Long days) {
+		log.info("Calculating fee: pax price {} for {} days", paxPrice, days);
 		final var totalPricePax = paxPrice.multiply(BigDecimal.valueOf(days));
+		final var fee = totalPricePax.multiply(this.fee);
 
-		return totalPricePax.multiply(this.fee);
+		log.info("Fee calculated: {}", fee);
+		return fee;
 	}
 }
